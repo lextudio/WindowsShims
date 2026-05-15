3d1a2d8a8e9e059ff79f23bf35e6ee499887a643
Lex Li <support@lextm.com>
2026-05-15 18:38:08 -0400

    Add DependencyProperties and accessors to TextElement for enhanced functionality

--- src/LeXtudio.Windows/System.Windows/Documents/TextElement.uno.cs
+++ src/LeXtudio.Windows/System.Windows/Documents/TextElement.uno.cs
@@ -4,6 +4,148 @@ namespace System.Windows.Documents;
 // satisfy interface contracts, so they must be provided as real methods.
 public abstract partial class TextElement : System.Windows.Input.IInputElement
 {
+    public static DependencyProperty AccessKeyProperty { get; } =
+        DependencyProperty.Register(nameof(AccessKey), typeof(string), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(string.Empty));
+
+    public static DependencyProperty AccessKeyScopeOwnerProperty { get; } =
+        DependencyProperty.Register(nameof(AccessKeyScopeOwner), typeof(DependencyObject), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(null));
+
+    public static DependencyProperty AllowFocusOnInteractionProperty { get; } =
+        DependencyProperty.Register(nameof(AllowFocusOnInteraction), typeof(bool), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(true));
+
+    public static DependencyProperty CharacterSpacingProperty { get; } =
+        DependencyProperty.Register(nameof(CharacterSpacing), typeof(int), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(0));
+
+    public static DependencyProperty ExitDisplayModeOnAccessKeyInvokedProperty { get; } =
+        DependencyProperty.Register(nameof(ExitDisplayModeOnAccessKeyInvoked), typeof(bool), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(true));
+
+    public static DependencyProperty FontStretchProperty { get; } =
+        DependencyProperty.Register(nameof(FontStretch), typeof(System.Windows.FontStretch), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(System.Windows.FontStretches.Normal));
+
+    public static DependencyProperty HorizontalTextAlignmentProperty { get; } =
+        DependencyProperty.Register(nameof(HorizontalTextAlignment), typeof(TextAlignment), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(TextAlignment.Left));
+
+    public static DependencyProperty IsAccessKeyScopeProperty { get; } =
+        DependencyProperty.Register(nameof(IsAccessKeyScope), typeof(bool), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(false));
+
+    public static DependencyProperty IsTextScaleFactorEnabledProperty { get; } =
+        DependencyProperty.Register(nameof(IsTextScaleFactorEnabled), typeof(bool), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(true));
+
+    public static DependencyProperty KeyTipHorizontalOffsetProperty { get; } =
+        DependencyProperty.Register(nameof(KeyTipHorizontalOffset), typeof(double), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(0d));
+
+    public static DependencyProperty KeyTipPlacementModeProperty { get; } =
+        DependencyProperty.Register(nameof(KeyTipPlacementMode), typeof(Microsoft.UI.Xaml.Input.KeyTipPlacementMode), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(Microsoft.UI.Xaml.Input.KeyTipPlacementMode.Auto));
+
+    public static DependencyProperty KeyTipVerticalOffsetProperty { get; } =
+        DependencyProperty.Register(nameof(KeyTipVerticalOffset), typeof(double), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(0d));
+
+    public static DependencyProperty LanguageProperty { get; } =
+        DependencyProperty.Register(nameof(Language), typeof(string), typeof(TextElement), new Microsoft.UI.Xaml.PropertyMetadata(string.Empty));
+
+    public string AccessKey
+    {
+        get => (string?)GetValue(AccessKeyProperty) ?? string.Empty;
+        set => SetValue(AccessKeyProperty, value);
+    }
+
+    public DependencyObject AccessKeyScopeOwner
+    {
+        get => (DependencyObject)GetValue(AccessKeyScopeOwnerProperty);
+        set => SetValue(AccessKeyScopeOwnerProperty, value);
+    }
+
+    public bool AllowFocusOnInteraction
+    {
+        get => (bool)GetValue(AllowFocusOnInteractionProperty);
+        set => SetValue(AllowFocusOnInteractionProperty, value);
+    }
+
+    public int CharacterSpacing
+    {
+        get => (int)GetValue(CharacterSpacingProperty);
+        set => SetValue(CharacterSpacingProperty, value);
+    }
+
+    public bool ExitDisplayModeOnAccessKeyInvoked
+    {
+        get => (bool)GetValue(ExitDisplayModeOnAccessKeyInvokedProperty);
+        set => SetValue(ExitDisplayModeOnAccessKeyInvokedProperty, value);
+    }
+
+    public System.Windows.FontStretch FontStretch
+    {
+        get => (System.Windows.FontStretch)GetValue(FontStretchProperty);
+        set => SetValue(FontStretchProperty, value);
+    }
+
+    public TextAlignment HorizontalTextAlignment
+    {
+        get => (TextAlignment)GetValue(HorizontalTextAlignmentProperty);
+        set => SetValue(HorizontalTextAlignmentProperty, value);
+    }
+
+    public bool IsAccessKeyScope
+    {
+        get => (bool)GetValue(IsAccessKeyScopeProperty);
+        set => SetValue(IsAccessKeyScopeProperty, value);
+    }
+
+    public bool IsTextScaleFactorEnabled
+    {
+        get => (bool)GetValue(IsTextScaleFactorEnabledProperty);
+        set => SetValue(IsTextScaleFactorEnabledProperty, value);
+    }
+
+    public double KeyTipHorizontalOffset
+    {
+        get => (double)GetValue(KeyTipHorizontalOffsetProperty);
+        set => SetValue(KeyTipHorizontalOffsetProperty, value);
+    }
+
+    public Microsoft.UI.Xaml.Input.KeyTipPlacementMode KeyTipPlacementMode
+    {
+        get => (Microsoft.UI.Xaml.Input.KeyTipPlacementMode)GetValue(KeyTipPlacementModeProperty);
+        set => SetValue(KeyTipPlacementModeProperty, value);
+    }
+
+    public double KeyTipVerticalOffset
+    {
+        get => (double)GetValue(KeyTipVerticalOffsetProperty);
+        set => SetValue(KeyTipVerticalOffsetProperty, value);
+    }
+
+    public string Language
+    {
+        get => (string?)GetValue(LanguageProperty) ?? string.Empty;
+        set => SetValue(LanguageProperty, value);
+    }
+
+    public string Name => string.Empty;
+
+    public Microsoft.UI.Xaml.XamlRoot XamlRoot { get; set; }
+
+    public event global::Windows.Foundation.TypedEventHandler<TextElement, Microsoft.UI.Xaml.Input.AccessKeyDisplayDismissedEventArgs> AccessKeyDisplayDismissed;
+    public event global::Windows.Foundation.TypedEventHandler<TextElement, Microsoft.UI.Xaml.Input.AccessKeyDisplayRequestedEventArgs> AccessKeyDisplayRequested;
+    public event global::Windows.Foundation.TypedEventHandler<TextElement, Microsoft.UI.Xaml.Input.AccessKeyInvokedEventArgs> AccessKeyInvoked;
+
+    public object FindName(string name) => null;
+
+    public bool Equals(TextElement other) => ReferenceEquals(this, other);
+
+    public override bool Equals(object obj) => ReferenceEquals(this, obj);
+
+    public override int GetHashCode() => base.GetHashCode();
+
+    protected void RaiseAccessKeyDisplayDismissed(Microsoft.UI.Xaml.Input.AccessKeyDisplayDismissedEventArgs args) =>
+        AccessKeyDisplayDismissed?.Invoke(this, args);
+
+    protected void RaiseAccessKeyDisplayRequested(Microsoft.UI.Xaml.Input.AccessKeyDisplayRequestedEventArgs args) =>
+        AccessKeyDisplayRequested?.Invoke(this, args);
+
+    protected void RaiseAccessKeyInvoked(Microsoft.UI.Xaml.Input.AccessKeyInvokedEventArgs args) =>
+        AccessKeyInvoked?.Invoke(this, args);
+
     bool System.Windows.Input.IInputElement.Focus() => true;
 
     void System.Windows.Input.IInputElement.RaiseEvent(RoutedEventArgs e) =>
